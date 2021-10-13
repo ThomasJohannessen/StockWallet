d2a5e345b505eb9bcdd786500e2de53960a983ea
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-10-13 16:58:33 +0200

    initial setup of Gradle and dependincies with Alpha vantage

--- app/src/main/java/no/stockwallet/MainActivity.java
+++ app/src/main/java/no/stockwallet/MainActivity.java
@@ -1,8 +1,14 @@
 package no.stockwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import yahoofinance.*;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.Config;
+import com.crazzyghost.alphavantage.parameters.Interval;
+import com.crazzyghost.alphavantage.parameters.OutputSize;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlphaVan();
     }
+public void AlphaVan() {
+    Config cfg = Config.builder()
+            .key("04C7U8DGXKH0OY8B")
+            .timeOut(10)
+            .build();
+
+
+}
+
 }
