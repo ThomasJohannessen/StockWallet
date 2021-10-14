cf2dcde8a97f4efab1457c5c690f2c255282aab8
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-10-14 18:10:30 +0200

    Fixed problem with brokerage to be added to avg price for stock

--- app/src/main/java/no/stockwallet/Investment.java
+++ app/src/main/java/no/stockwallet/Investment.java
@@ -1,18 +1,18 @@
 package no.stockwallet;
 
-import java.util.ArrayList;
-
 public class Investment {
 
     String ticker,currency;
-    double price, brokerage;
+    double price;
     int volum;
 
     public Investment(String ticker, int volum, double price, String currency, double brokerage) {
         this.ticker = ticker;
         this.volum = volum;
-        this.price = price;
         this.currency = currency;
-        this.brokerage = brokerage;
+
+        double brokeragePrStock = brokerage / this.volum;
+
+        this.price = price + brokeragePrStock;
     }
 }
