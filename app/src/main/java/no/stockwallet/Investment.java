bff6817049dfe02a7ee19cd3e6b924d1d81103d4
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-10-14 16:03:52 +0200

    fixed the investment class

--- app/src/main/java/no/stockwallet/Investment.java
+++ app/src/main/java/no/stockwallet/Investment.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Investment {
 
-    public ArrayList<Investment> investments = new ArrayList<Investment>();
+    String ticker,currency;
+    double price, brokerage;
+    int volum;
 
     public Investment(String ticker, int volum, double price, String currency, double brokerage) {
-        Investment investment = new Investment(ticker,volum,price,currency,brokerage);
-        investment.investments.add(investment);
-    }
-
-    public ArrayList<Investment> getInvestments() {
-        return investments;
+        this.ticker = ticker;
+        this.volum = volum;
+        this.price = price;
+        this.currency = currency;
+        this.brokerage = brokerage;
     }
 }
-
