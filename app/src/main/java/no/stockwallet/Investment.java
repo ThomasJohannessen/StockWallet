188450297abc3d9717eb9b6ef9e2cea8f2138296
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-10-14 15:04:05 +0200

    fixed investment class

--- app/src/main/java/no/stockwallet/Investment.java
+++ app/src/main/java/no/stockwallet/Investment.java
@@ -6,17 +6,13 @@ public class Investment {
 
     public ArrayList<Investment> investments = new ArrayList<Investment>();
 
-    public Investment(String ticker, int volum, float kurs) {
-        Investment investment = new Investment(ticker, volum, kurs);
+    public Investment(String ticker, int volum, double price, String currency, double brokerage) {
+        Investment investment = new Investment(ticker,volum,price,currency,brokerage);
         investment.investments.add(investment);
-    }
-
-    public void addTrade(String ticker, int volum, float kurs) {
-        Investment trade = new Investment(ticker, volum, kurs);
-        investments.add(trade);
     }
 
     public ArrayList<Investment> getInvestments() {
         return investments;
     }
 }
+
