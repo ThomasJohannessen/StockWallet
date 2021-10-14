4d5f5a28bfa6776cbfb0d3216bd38f4747663363
Michal Kowalski <michalk@hiof.no>
2021-10-14 16:31:39 +0200

    implementing cards in recycler view, need styling

--- app/src/main/java/no/stockwallet/Fragments/OverviewFragments/StockRecyclerAdapter.java
+++ app/src/main/java/no/stockwallet/Fragments/OverviewFragments/StockRecyclerAdapter.java
@@ -3,6 +3,7 @@ package no.stockwallet.Fragments.OverviewFragments;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +13,15 @@ import no.stockwallet.R;
 public class StockRecyclerAdapter extends RecyclerView.Adapter<StockRecyclerAdapter.ViewHolder>{
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView stockNameView, stockValueView, stockPercentEarning, stockEarning;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            stockNameView = itemView.findViewById(R.id.StockNamePH);
+            stockValueView = itemView.findViewById(R.id.StockValuePH);
+            stockPercentEarning = itemView.findViewById(R.id.StockEarningPCPH);
+            stockEarning = itemView.findViewById(R.id.StockEarningPH);
         }
     }
 
@@ -28,12 +35,15 @@ public class StockRecyclerAdapter extends RecyclerView.Adapter<StockRecyclerAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.stockEarning.setText("Earning" + String.valueOf(position));
+        holder.stockNameView.setText("StockName" + String.valueOf(position));
+        holder.stockValueView.setText("Value" + String.valueOf(position));
+        holder.stockPercentEarning.setText("Percent" + String.valueOf(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 15;
     }
 
 }
