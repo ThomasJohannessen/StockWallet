43d60473a70bd09a5adfb572a75be26563a8b9be
Michal Kowalski <michalk@hiof.no>
2021-10-14 16:02:55 +0200

    creating layout for overview card items and template adapter for cards

--- app/src/main/java/no/stockwallet/Fragments/OverviewFragments/StockRecyclerAdapter.java
+++ app/src/main/java/no/stockwallet/Fragments/OverviewFragments/StockRecyclerAdapter.java
@@ -1,4 +1,39 @@
 package no.stockwallet.Fragments.OverviewFragments;
 
-public class StockRecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import no.stockwallet.R;
+
+public class StockRecyclerAdapter extends RecyclerView.Adapter<StockRecyclerAdapter.ViewHolder>{
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.stock_card_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
