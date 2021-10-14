286d3ab9077a6ed4bba6c8fb713d4d3a6d6e77af
Michal Kowalski <michalk@hiof.no>
2021-10-14 14:25:20 +0200

    adding drawer layout to main activity

--- app/src/main/java/no/stockwallet/MainActivity.java
+++ app/src/main/java/no/stockwallet/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home_fragments_wrapper);
+        setContentView(R.layout.activity_main);
 
     }
 
