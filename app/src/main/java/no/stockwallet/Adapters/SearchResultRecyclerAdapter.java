ab62b7b9e75d511a5931cae8948d90879c8cdbb5
Michal Kowalski <michalk@hiof.no>
2021-10-22 19:08:27 +0200

    Adding dummydata to recycler view in search fragment

--- app/src/main/java/no/stockwallet/Adapters/SearchResultRecyclerAdapter.java
+++ app/src/main/java/no/stockwallet/Adapters/SearchResultRecyclerAdapter.java
@@ -6,13 +6,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import no.stockwallet.Investment;
+
 public class SearchResultRecyclerAdapter extends RecyclerView.Adapter<SearchResultRecyclerAdapter.ViewHolder>{
+    private ArrayList<Investment> data;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    public SearchResultRecyclerAdapter() {
+        initDummyData();
+    }
+
+    private void initDummyData() {
+        data = new ArrayList<>();
+
+        data.add(new Investment("NHY.OL",200,60,"NOK",1));
+        data.add(new Investment("AKH.OL",100,30,"NOK",10));
+        data.add(new Investment("NOD.OL",10,250,"NOK",100));
     }
 
     @NonNull
