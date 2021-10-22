90378ec1fabcb55620405526501720fc419e3a6f
Michal Kowalski <michalk@hiof.no>
2021-10-22 21:13:25 +0200

    recycler view set up and works with dummy data

--- app/src/main/java/no/stockwallet/Adapters/SearchResultRecyclerAdapter.java
+++ app/src/main/java/no/stockwallet/Adapters/SearchResultRecyclerAdapter.java
@@ -1,23 +1,36 @@
 package no.stockwallet.Adapters;
 
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 import no.stockwallet.Investment;
+import no.stockwallet.R;
 
 public class SearchResultRecyclerAdapter extends RecyclerView.Adapter<SearchResultRecyclerAdapter.ViewHolder>{
     private ArrayList<Investment> data;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView dummy1;
+        private TextView dummy2;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            dummy1 = itemView.findViewById(R.id.dummyName);
+            dummy2 = itemView.findViewById(R.id.dummyValue);
         }
+
+        public TextView getDummy1() {return dummy1;}
+        public TextView getDummy2() {return dummy2;}
     }
 
     public SearchResultRecyclerAdapter() {
@@ -35,17 +48,20 @@ public class SearchResultRecyclerAdapter extends RecyclerView.Adapter<SearchResu
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Investment object = data.get(position);
+        holder.getDummy1().setText(object.getTicker());
+        holder.getDummy2().setText(String.valueOf(object.getPrice()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
 }
