33469f5abd10d2fb71b8566818102340ace79879
Michal Kowalski <michalk@hiof.no>
2021-10-29 13:42:38 +0200

    adding scrollview to login screen

--- app/src/main/java/no/stockwallet/RegisterUserActivity.java
+++ app/src/main/java/no/stockwallet/RegisterUserActivity.java
@@ -9,5 +9,7 @@ public class RegisterUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+
     }
 }
