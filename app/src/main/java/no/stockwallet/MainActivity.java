aeb4c550cf14a33f5789f359a1a8d4632746ec40
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-10-31 19:08:26 +0100

    removed testing code

--- app/src/main/java/no/stockwallet/MainActivity.java
+++ app/src/main/java/no/stockwallet/MainActivity.java
@@ -9,16 +9,13 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.Gravity;
 import android.widget.TextView;
 
-import com.crazzyghost.alphavantage.*;
 import com.google.android.material.navigation.NavigationView;
 
-
-import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     // TODO Recycler view i søk - oppdatert dynamisk fra api
@@ -38,34 +35,15 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(StockViewModel.class);
         viewModel.fillWithDummyData();
 
-        Map<String, BigDecimal> aksje = new HashMap<>();
-        StockDataRetriever.getInstance().getStockPrice(aksje,"NHY.OL");
-
-        /*while (aksje.isEmpty()) {
-            try { TimeUnit.MILLISECONDS.sleep(10); }
-            catch (InterruptedException e) { e.printStackTrace(); }
-        }
-
-        Log.d("Ferdig-enkel", String.valueOf(aksje));*/
-
-        Map<String, BigDecimal> aksjer = new HashMap<>();
-        StockDataRetriever.getInstance().getMultipleStockPrices(aksjer,viewModel.getInvestmentTickers());
-
-        /*while (aksjer.isEmpty()) {
-            try { TimeUnit.MILLISECONDS.sleep(10); }
-            catch (InterruptedException e) { e.printStackTrace(); }
-        }
-
-        Log.d("Ferdig-flere", String.valueOf(aksjer));*/
 
         findViewById(R.id.NavMenuButton).setOnClickListener((view) -> {
             DrawerLayout drawer = findViewById(R.id.drawer_layout);
             drawer.openDrawer(Gravity.LEFT);
         });
-
     }
 
-    public void setToolbarTitle(String title){
-        TextView navTitle = (TextView) findViewById (R.id.NavBar_Title);
+    public void setToolbarTitle(String title) {
+        TextView navTitle = (TextView) findViewById(R.id.NavBar_Title);
         navTitle.setText(title);
     }
+}
