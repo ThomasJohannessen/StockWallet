5355a0038a173f42218f52fa7e55dbbaa88b2a56
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-11-03 15:15:05 +0100

    Done the gainfargent so it shows the real data

--- app/src/main/java/no/stockwallet/Fragments/HomeFragments/GainFragment.java
+++ app/src/main/java/no/stockwallet/Fragments/HomeFragments/GainFragment.java
@@ -6,20 +6,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import no.stockwallet.Fragments.Wrappers.HomeFragmentsWrapper;
 import no.stockwallet.Investment;
 import no.stockwallet.MainActivity;
 import no.stockwallet.R;
+import no.stockwallet.StockCalculations;
+import no.stockwallet.StockDataRetriever;
 
 public class GainFragment extends Fragment {
-    private TextView tempView, tempView2, tempView3, tempView4, tempView5, tempView6;
-
 
     public GainFragment() {}
 
@@ -32,25 +40,86 @@ public class GainFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+         TextView stockName1, stockName2, stockName3, stockName4, stockName5, stockName6,
+                stockPrcnt1, stockPrcnt2, stockPrcnt3, stockPrcnt4, stockPrcnt5, stockPrcnt6,
+                stockPrice1, stockPrice2, stockPrice3, stockPrice4, stockPrice5, stockPrice6;
+
         super.onViewCreated(view, savedInstanceState);
+        HomeFragmentsWrapper parent = (HomeFragmentsWrapper) getParentFragment();
 
-        /*ArrayList<Investment> data = new ArrayList<>(MainActivity.investments.values());
+        //goes thorough all the investments and finds the biggest loss and winner and puts them in array getTop.. and getBottom..
+        HashMap<String, Investment> investments = parent.getViewModel().getStockMap().getValue();
+        StockCalculations.getInstance().getIntradayChangesInStocksPercent(investments);
+
+        ArrayList<Pair<String, BigDecimal>> topp3Arr = StockCalculations.getInstance().getTopThreeGainerStocks();
+        ArrayList<Pair<String, BigDecimal>> bottom3Arr = StockCalculations.getInstance().getBottomThreeLoserStocks();
+
+        String[] tickers = new String[6];
+
+        for(int i=0; i<6; i++){
+            if (i<3) {
+                tickers[i] = bottom3Arr.get(i).first;
+            }else{
+                tickers[i] = topp3Arr.get(i-3).first;
+            }
+        }
+
+        HashMap<String,BigDecimal> stockPrices = new HashMap<>();
+        StockDataRetriever.getInstance().getMultipleStockPrices(stockPrices,tickers);
+
+        while (stockPrices.size() < tickers.length){
+            try {TimeUnit.MILLISECONDS.sleep(10);}
+            catch (InterruptedException e) {e.printStackTrace();}
+        }
+
+        stockPrcnt1 = view.findViewById(R.id.stockPCR1);
+        stockName1 = view.findViewById(R.id.stockNameR1);
+        stockPrice1 = view.findViewById(R.id.stockPriceR1);
+        Log.d("TotaltInvestert-ticker",topp3Arr.get(0).first);
+        Log.d("TotaltInvestert-Økning", String.valueOf(topp3Arr.get(0).second));
 
 
-        tempView = view.findViewById(R.id.stockNameR1);
-        tempView2 = view.findViewById(R.id.stockPCR1);
-        tempView3 = view.findViewById(R.id.stockPriceR1);
+        stockPrcnt1.setText(String.valueOf(topp3Arr.get(0).second + " %"));
+        stockName1.setText(String.valueOf(topp3Arr.get(0).first));
+        stockPrice1.setText(String.valueOf(stockPrices.get(topp3Arr.get(0).first) + " " + investments.get(topp3Arr.get(0).first).getCurrency()));
 
-        tempView4 = view.findViewById(R.id.stockNameR4);
-        tempView5 = view.findViewById(R.id.stockPCR4);
-        tempView6 = view.findViewById(R.id.stockPriceR4);
+        stockPrcnt2 = view.findViewById(R.id.stockPCR2);
+        stockName2 = view.findViewById(R.id.stockNameR2);
+        stockPrice2 = view.findViewById(R.id.stockPriceR2);
+        stockPrcnt2.setText(String.valueOf(topp3Arr.get(1).second + " %"));
+        stockName2.setText(String.valueOf(topp3Arr.get(1).first));
+        stockPrice2.setText(String.valueOf(stockPrices.get(topp3Arr.get(1).first) + " " + investments.get(topp3Arr.get(1).first).getCurrency()));
 
-        tempView.setText(data.get(0).getTicker());
-        tempView2.setText("+20%");
-        tempView3.setText(String.valueOf(data.get(0).getPrice()) + " " + data.get(0).getCurrency());
 
-        tempView4.setText(data.get(2).getTicker());
-        tempView5.setText("-25%");
-        tempView6.setText(String.valueOf(data.get(2).getPrice()) + " " + data.get(2).getCurrency());*/
+        stockPrcnt3 = view.findViewById(R.id.stockPCR3);
+        stockName3 = view.findViewById(R.id.stockNameR3);
+        stockPrice3 = view.findViewById(R.id.stockPriceR3);
+        stockPrcnt3.setText(String.valueOf(topp3Arr.get(2).second + " %"));
+        stockName3.setText(String.valueOf(topp3Arr.get(2).first));
+        stockPrice3.setText(String.valueOf(stockPrices.get(topp3Arr.get(2).first) + " " + investments.get(topp3Arr.get(2).first).getCurrency()));
+
+
+        stockPrcnt4 = view.findViewById(R.id.stockPCR4);
+        stockName4 = view.findViewById(R.id.stockNameR4);
+        stockPrice4 = view.findViewById(R.id.stockPriceR4);
+        stockPrcnt4.setText(String.valueOf(bottom3Arr.get(0).second + " %"));
+        stockName4.setText(String.valueOf(bottom3Arr.get(0).first));
+        stockPrice4.setText(String.valueOf(stockPrices.get(bottom3Arr.get(0).first) + " " + investments.get(bottom3Arr.get(0).first).getCurrency()));
+
+        stockPrcnt5 = view.findViewById(R.id.stockPCR5);
+        stockName5 = view.findViewById(R.id.stockNameR5);
+        stockPrice5 = view.findViewById(R.id.stockPriceR5);
+        stockPrcnt5.setText(String.valueOf(bottom3Arr.get(1).second + " %"));
+        stockName5.setText(String.valueOf(bottom3Arr.get(1).first));
+        stockPrice5.setText(String.valueOf(stockPrices.get(bottom3Arr.get(1).first) + " " + investments.get(bottom3Arr.get(1).first).getCurrency()));
+
+
+        stockPrcnt6 = view.findViewById(R.id.stockPCR6);
+        stockName6 = view.findViewById(R.id.stockNameR6);
+        stockPrice6 = view.findViewById(R.id.stockPriceR6);
+        stockPrcnt6.setText(String.valueOf(bottom3Arr.get(2).second + " %"));
+        stockName6.setText(String.valueOf(bottom3Arr.get(2).first));
+        stockPrice6.setText(String.valueOf(stockPrices.get(bottom3Arr.get(2).first) + " " + investments.get(bottom3Arr.get(2).first).getCurrency()));
+
     }
 }
