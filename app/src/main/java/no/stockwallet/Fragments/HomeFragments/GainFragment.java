3c689d98fadedce483a3557796ab11727c8cc1d0
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-11-04 20:25:50 +0100

    added logic for  showing error when not 6 stocks and error msg

--- app/src/main/java/no/stockwallet/Fragments/HomeFragments/GainFragment.java
+++ app/src/main/java/no/stockwallet/Fragments/HomeFragments/GainFragment.java
@@ -43,8 +43,11 @@ public class GainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
          TextView stockName1, stockName2, stockName3, stockName4, stockName5, stockName6,
                 stockPrcnt1, stockPrcnt2, stockPrcnt3, stockPrcnt4, stockPrcnt5, stockPrcnt6,
-                stockPrice1, stockPrice2, stockPrice3, stockPrice4, stockPrice5, stockPrice6;
+                stockPrice1, stockPrice2, stockPrice3, stockPrice4, stockPrice5, stockPrice6,
+                errMsg1,errMsg2;
+
         DecimalFormat df = new DecimalFormat("#.##");
+        Boolean underSixInvestments = false;
 
         super.onViewCreated(view, savedInstanceState);
         HomeFragmentsWrapper parent = (HomeFragmentsWrapper) getParentFragment();
@@ -59,6 +62,9 @@ public class GainFragment extends Fragment {
         String[] tickers = new String[6];
 
         for(int i=0; i<6; i++){
+            if (bottom3Arr.get(i).first == null){
+                underSixInvestments = true;
+            }
             if (i<3) {
                 tickers[i] = bottom3Arr.get(i).first;
             }else{
@@ -66,58 +72,88 @@ public class GainFragment extends Fragment {
             }
         }
 
-        HashMap<String,BigDecimal> stockPrices = new HashMap<>();
-        StockDataRetriever.getInstance().getMultipleStockPrices(stockPrices,tickers);
-
-        while (stockPrices.size() < tickers.length){
-            try {TimeUnit.MILLISECONDS.sleep(10);}
-            catch (InterruptedException e) {e.printStackTrace();}
-        }
-
         stockPrcnt1 = view.findViewById(R.id.stockPCR1);
         stockName1 = view.findViewById(R.id.stockNameR1);
         stockPrice1 = view.findViewById(R.id.stockPriceR1);
-
-        stockPrcnt1.setText(String.valueOf(topp3Arr.get(0).second + " %"));
-        stockName1.setText(String.valueOf(topp3Arr.get(0).first));
-        stockPrice1.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(0).first))) + " " + investments.get(topp3Arr.get(0).first).getCurrency()));
-
         stockPrcnt2 = view.findViewById(R.id.stockPCR2);
         stockName2 = view.findViewById(R.id.stockNameR2);
         stockPrice2 = view.findViewById(R.id.stockPriceR2);
-        stockPrcnt2.setText(String.valueOf(topp3Arr.get(1).second + " %"));
-        stockName2.setText(String.valueOf(topp3Arr.get(1).first));
-        stockPrice2.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(1).first))) + " " + investments.get(topp3Arr.get(1).first).getCurrency()));
-
-
         stockPrcnt3 = view.findViewById(R.id.stockPCR3);
         stockName3 = view.findViewById(R.id.stockNameR3);
         stockPrice3 = view.findViewById(R.id.stockPriceR3);
-        stockPrcnt3.setText(String.valueOf(topp3Arr.get(2).second + " %"));
-        stockName3.setText(String.valueOf(topp3Arr.get(2).first));
-        stockPrice3.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(2).first))) + " " + investments.get(topp3Arr.get(2).first).getCurrency()));
-
-
         stockPrcnt4 = view.findViewById(R.id.stockPCR4);
         stockName4 = view.findViewById(R.id.stockNameR4);
         stockPrice4 = view.findViewById(R.id.stockPriceR4);
-        stockPrcnt4.setText(String.valueOf(bottom3Arr.get(0).second + " %"));
-        stockName4.setText(String.valueOf(bottom3Arr.get(0).first));
-        stockPrice4.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(0).first))) + " " + investments.get(bottom3Arr.get(0).first).getCurrency()));
-
         stockPrcnt5 = view.findViewById(R.id.stockPCR5);
         stockName5 = view.findViewById(R.id.stockNameR5);
         stockPrice5 = view.findViewById(R.id.stockPriceR5);
-        stockPrcnt5.setText(String.valueOf(bottom3Arr.get(1).second + " %"));
-        stockName5.setText(String.valueOf(bottom3Arr.get(1).first));
-        stockPrice5.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(1).first))) + " " + investments.get(bottom3Arr.get(1).first).getCurrency()));
-
-
         stockPrcnt6 = view.findViewById(R.id.stockPCR6);
         stockName6 = view.findViewById(R.id.stockNameR6);
         stockPrice6 = view.findViewById(R.id.stockPriceR6);
-        stockPrcnt6.setText(String.valueOf(bottom3Arr.get(2).second + " %"));
-        stockName6.setText(String.valueOf(bottom3Arr.get(2).first));
-        stockPrice6.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(2).first))) + " " + investments.get(bottom3Arr.get(2).first).getCurrency()));
+
+
+        if (underSixInvestments){
+            stockPrcnt1.setText("");
+            stockName1.setText("");
+            stockPrice1.setText("");
+            stockPrcnt2.setText("");
+            stockName2.setText("");
+            stockPrice2.setText("");
+            stockPrcnt3.setText("");
+            stockName3.setText("");
+            stockPrice3.setText("");
+            stockPrcnt4.setText("");
+            stockName4.setText("");
+            stockPrice4.setText("");
+            stockPrcnt5.setText("");
+            stockName5.setText("");
+            stockPrice5.setText("");
+            stockPrcnt6.setText("");
+            stockName6.setText("");
+            stockPrice6.setText("");
+
+            errMsg1 = view.findViewById(R.id.ErrorMsgTooFewStocks);
+            errMsg2 = view.findViewById(R.id.ErrorMsgTooFewStocks2);
+            errMsg1.setText("Du må ha 6 aksjer i din portefølge for å bruke denne funksjonen.");
+            errMsg2.setText("Du må ha 6 aksjer i din portefølge for å bruke denne funksjonen.");
+
+        }
+        else {
+            HashMap<String, BigDecimal> stockPrices = new HashMap<>();
+            StockDataRetriever.getInstance().getMultipleStockPrices(stockPrices, tickers);
+
+            while (stockPrices.size() < tickers.length) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            stockPrcnt1.setText(String.valueOf(topp3Arr.get(0).second + " %"));
+            stockName1.setText(String.valueOf(topp3Arr.get(0).first));
+            stockPrice1.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(0).first))) + " " + investments.get(topp3Arr.get(0).first).getCurrency()));
+
+            stockPrcnt2.setText(String.valueOf(topp3Arr.get(1).second + " %"));
+            stockName2.setText(String.valueOf(topp3Arr.get(1).first));
+            stockPrice2.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(1).first))) + " " + investments.get(topp3Arr.get(1).first).getCurrency()));
+
+            stockPrcnt3.setText(String.valueOf(topp3Arr.get(2).second + " %"));
+            stockName3.setText(String.valueOf(topp3Arr.get(2).first));
+            stockPrice3.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(topp3Arr.get(2).first))) + " " + investments.get(topp3Arr.get(2).first).getCurrency()));
+
+            stockPrcnt4.setText(String.valueOf(bottom3Arr.get(0).second + " %"));
+            stockName4.setText(String.valueOf(bottom3Arr.get(0).first));
+            stockPrice4.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(0).first))) + " " + investments.get(bottom3Arr.get(0).first).getCurrency()));
+
+            stockPrcnt5.setText(String.valueOf(bottom3Arr.get(1).second + " %"));
+            stockName5.setText(String.valueOf(bottom3Arr.get(1).first));
+            stockPrice5.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(1).first))) + " " + investments.get(bottom3Arr.get(1).first).getCurrency()));
+
+            stockPrcnt6.setText(String.valueOf(bottom3Arr.get(2).second + " %"));
+            stockName6.setText(String.valueOf(bottom3Arr.get(2).first));
+            stockPrice6.setText(String.valueOf(Double.parseDouble(df.format(stockPrices.get(bottom3Arr.get(2).first))) + " " + investments.get(bottom3Arr.get(2).first).getCurrency()));
+        }
     }
 }
