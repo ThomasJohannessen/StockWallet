e240dcdae224e4265b4e280a9839857ef356085e
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-11-05 17:54:18 +0100

    new metods to get for singel stock

--- app/src/main/java/no/stockwallet/API_InvestmentDataHandler.java
+++ app/src/main/java/no/stockwallet/API_InvestmentDataHandler.java
@@ -2,6 +2,7 @@ package no.stockwallet;
 
 import android.util.Log;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -10,6 +11,7 @@ import yahoofinance.Stock;
 public class API_InvestmentDataHandler {
 
     StockViewModel svm;
+    HashMap<String, BigDecimal> stockPricesTopp3Bottom3 = new HashMap<>();
 
     public API_InvestmentDataHandler(StockViewModel viewModel) {
         svm = viewModel;
@@ -96,6 +98,29 @@ public class API_InvestmentDataHandler {
         thread.start();
     }
 
+    public void addTotalEarningsNOKOnSingeStockToInvestment(Investment investment){
+        Thread thread = new Thread(() -> {
+
+            String ticker = investment.getTicker();
+
+            HashMap<String, Investment> stock = new HashMap<>();
+            stock.put(investment.getTicker(),investment);
+
+            double earnings = 0.00000000000001;
+            earnings = StockCalculations.getInstance().getEarningsNOKSingleStock(stock,ticker);
+
+            while (earnings == 0.00000000000001) {
+                try {TimeUnit.MILLISECONDS.sleep(10);}
+                catch (InterruptedException e) {e.printStackTrace();}
+            }
+
+            Investment updateInvestment = svm.getInvestment(ticker);
+            updateInvestment.setTotalEarningsStockNOK(earnings);
+
+        });
+        thread.start();
+    }
+
     public void addTotalEarningsPercentOnStockToInvestment(){
         Thread thread = new Thread(new Runnable() {
             @Override
@@ -124,6 +149,29 @@ public class API_InvestmentDataHandler {
         });
         thread.start();
     }
+    public void addTotalEarningsPercentOnSingeStockToInvestment(Investment investment){
+        Thread thread = new Thread(() -> {
+
+            String ticker = investment.getTicker();
+
+            HashMap<String, Investment> stock = new HashMap<>();
+            stock.put(investment.getTicker(),investment);
+
+            double earnings = 0.00000000000001;
+            earnings = StockCalculations.getInstance().getEarningsPercentSingleStock(stock,ticker);
+
+            while (earnings == 0.00000000000001) {
+                try {TimeUnit.MILLISECONDS.sleep(10);}
+                catch (InterruptedException e) {e.printStackTrace();}
+            }
+
+            Investment updateInvestment = svm.getInvestment(ticker);
+            updateInvestment.setTotalEarningsStockPercent(earnings);
+
+        });
+        thread.start();
+    }
+
 
     public void addTotalMarkedValueNOKOnStockToInvestment(){
         Thread thread = new Thread(new Runnable() {
@@ -152,5 +200,58 @@ public class API_InvestmentDataHandler {
             }
         });
         thread.start();
+    }
+    public void addTotalMarkedValueNOKOnSingeStockToInvestment(Investment investment){
+        Thread thread = new Thread(() -> {
+
+            String ticker = investment.getTicker();
+
+            HashMap<String, Investment> stock = new HashMap<>();
+            stock.put(investment.getTicker(),investment);
+
+            double marketValue = 0.00000000000001;
+            marketValue = StockCalculations.getInstance().getMarkedValueNOKSingleStock(stock,ticker);
+
+            while (marketValue == 0.00000000000001) {
+                try {TimeUnit.MILLISECONDS.sleep(10);}
+                catch (InterruptedException e) {e.printStackTrace();}
+            }
+
+            Investment updateInvestment = svm.getInvestment(ticker);
+            updateInvestment.setTotalEarningsStockPercent(marketValue);
+
+        });
+        thread.start();
+    }
+
+    public void findTopp3AndBootom3() {
+        //goes thorough all the investments and finds the biggest loss and winner and puts them in array getTop.. and getBottom..
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                HashMap<String, Investment> investments = svm.getStockMap().getValue();
+                if(investments.size() != 0) {
+                    StockCalculations.getInstance().getIntradayChangesInStocksPercent(investments);
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public void fetchStockPricesForToppAndBottom3(String[] tickers) {
+        //fetches the stockprices for topp3 and bottom3, and saves them in hashmap for later getmetode
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                StockDataRetriever.getInstance().getMultipleStockPrices(stockPricesTopp3Bottom3, tickers);
+            }
+        });
+        thread.start();
+
+    }
+
+    public HashMap<String, BigDecimal> getStockPricesTopp3Bottom3Map() {
+        return stockPricesTopp3Bottom3;
     }
 }
