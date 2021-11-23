64c6d7be6989cedb1820404212ae857b70df6843
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-11-23 14:57:03 +0100

    added graph handling and viwer

--- app/src/main/java/no/stockwallet/Fragments/DetailedStockFragments/GraphFragment.java
+++ app/src/main/java/no/stockwallet/Fragments/DetailedStockFragments/GraphFragment.java
@@ -7,11 +7,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,12 +25,11 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
-import no.stockwallet.Adapters.SearchResultRecyclerAdapter;
 import no.stockwallet.Fragments.Wrappers.DetailStockFragmentsWrapper;
-import no.stockwallet.Fragments.Wrappers.HomeFragmentsWrapper;
 import no.stockwallet.R;
 import no.stockwallet.Support.JsonSupport;
 import no.stockwallet.ViewModels.StockViewModel;
@@ -43,8 +41,8 @@ public class GraphFragment extends Fragment {
     private String QUEUE_TAG = "DETAILEDGRAPHAPI";
     private View view;
     private StockViewModel viewModel;
+    TextView errMsg1;
     ArrayList<Double> historyValues = new ArrayList<>();
-    LineGraphSeries<DataPoint> series;
 
     public GraphFragment() {
         // Required empty public constructor
@@ -68,51 +66,81 @@ public class GraphFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
-
-        GraphView graph = (GraphView) view.findViewById(R.id.graphDetailed);
 
         Bundle parentArguments = getParentFragment().getArguments();
         HashMap<String, Stock> map = (HashMap<String, Stock>) parentArguments.getSerializable("hashmap");
         Stock stock = map.get("Stock");
         queue = Volley.newRequestQueue(getContext());
-        requestHistorydata("stock.getSymbol()", view);
 
+        if (!stock.getSymbol().equals("")) {
+            String baseURL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&outputsize=compact&apikey=04C7U8DGXKH0OY8B&symbol=";
+            String url = baseURL + stock.getSymbol();
+            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
 
-        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
-        graph.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.HORIZONTAL );
-        graph.setVisibility(View.VISIBLE);
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("{}")) {
+                        errMsg1 = view.findViewById(R.id.errorGraph);
+                        errMsg1.setVisibility(View.VISIBLE);
+                        Snackbar.make(view, "Denne aksjen har dessverre ikke støtte for historisk visning", Snackbar.LENGTH_LONG).show();
+                    }else{
+                        try {
+                            historyValues = JsonSupport.getInstance().jsonToPairArrayGraph(response);
+                        } catch (Exception e) {
+                            Snackbar.make(view, "Error in response from server, try again later", Snackbar.LENGTH_SHORT).show();
+                        }
 
-        DataPoint[] dataPoints = new DataPoint[historyValues.size()];
+                        GraphView graph = (GraphView) view.findViewById(R.id.graphDetailed);
+                        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+                        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
+                        graph.setVisibility(View.VISIBLE);
 
-        for (int i=0; i<historyValues.size();i++){
-            Double temp = historyValues.get(i);
-            dataPoints[i] = new DataPoint(i, temp);
+                        graph.addSeries(makeDataPoints());
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("RESPONSE", error.toString());
+                    Snackbar.make(view, "Something went wrong, check your internet connection", Snackbar.LENGTH_SHORT).show();
+                }
+            });
+            request.setTag(QUEUE_TAG);
+            queue.add(request);
         }
-
-        series = new LineGraphSeries<DataPoint>(dataPoints);
-
-        graph.addSeries(series);
-
-
     }
 
 
-    private void requestHistorydata(String keyword, View view) {
+    private LineGraphSeries<DataPoint> makeDataPoints(){
+
+        DataPoint[] dataPoints = new DataPoint[historyValues.size()];
+
+        for(int i = historyValues.size()-1; i >= 0; i--){
+            Double temp = historyValues.get(i);
+            dataPoints[i] = new DataPoint(i, temp);
+        }
+        return new LineGraphSeries<DataPoint>(dataPoints);
+    }
+
+    private void requestHistorydata(String keyword) {
         if (keyword.equals("") == false) {
             new Thread(() -> {
                 String baseURL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&outputsize=compact&apikey=04C7U8DGXKH0OY8B&symbol=";
                 String url = baseURL + keyword;
+                Log.d("ARESPONSE123", "starter henting");
                 StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
                     @Override
                     public void onResponse(String response) {
                         try {
                             Log.d("RESPONSE", response);
                             historyValues = JsonSupport.getInstance().jsonToPairArrayGraph(response);
+                            Log.d("ARESPONSE123", "ferig hentet");
                             Log.d("ARESPONSE123return", String.valueOf(historyValues));
+
 
                         } catch (Exception e) {
                             Snackbar.make(view, "Error in response from server, try again later", Snackbar.LENGTH_SHORT).show();
@@ -121,7 +149,7 @@ public class GraphFragment extends Fragment {
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        //Log.d("ARESPONSE", error.toString());
+                        Log.d("ARESPONSE", error.toString());
                         Snackbar.make(view, "Something went wrong, check your internet connection", Snackbar.LENGTH_SHORT).show();
                     }
                 });
